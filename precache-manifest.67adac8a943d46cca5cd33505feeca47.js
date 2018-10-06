4bd807c03831001eef27073adae80da1cc5a2ec0
AKH000 <andoniabedul@gmail.com>
2018-10-06 20:21:56 +0200

    Updates

---
+++ precache-manifest.67adac8a943d46cca5cd33505feeca47.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/weather-react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "eb5d4223e9399d7b7e6a",
+    "url": "/weather-react-app/static/css/main.c9f8ad88.chunk.css"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-react-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "55204936964d65f3e6c4",
+    "url": "/weather-react-app/static/js/1.55204936.chunk.js"
+  },
+  {
+    "revision": "d01157d07cdf0ece631f",
+    "url": "/weather-react-app/static/js/runtime~main.d01157d0.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/weather-react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/weather-react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/weather-react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "eb5d4223e9399d7b7e6a",
+    "url": "/weather-react-app/static/js/main.eb5d4223.chunk.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/weather-react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-react-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-react-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-react-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-react-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "55204936964d65f3e6c4",
+    "url": "/weather-react-app/static/css/1.45a0f128.chunk.css"
+  },
+  {
+    "revision": "a5f57f98cc09d0a093bf0a2bd4855b5c",
+    "url": "/weather-react-app/index.html"
+  }
+];
